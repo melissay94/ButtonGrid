b231d6583039bbb3e73a73285a0057fd374fee58
Melissa Young <melissay94@yahoo.com>
2020-03-29 21:32:10 -0700

    Created footer

--- button-grid/src/components/Footer.tsx
+++ button-grid/src/components/Footer.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Navbar, Nav, NavbarText, NavItem, NavLink } from "reactstrap";
 
 function Footer() {
   return(
-    <div>Footer</div>
+    <Navbar>
+      <NavbarText>Copyright © 2020, Melissa Young</NavbarText>
+      <Nav>
+        <NavItem>
+          <NavLink href="https://github.com/melissay94/ButtonGrid">ButtonGrid GitHub</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://linkedin.com/in/melissadcyoung/">My LinkedIn</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 }
 
